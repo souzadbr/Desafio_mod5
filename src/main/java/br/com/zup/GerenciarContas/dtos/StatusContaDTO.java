114bbfebd43a8b966756381f4caf0f587188e99a
Debora Souza <deboral.p@hotmail.com>
2021-11-10 16:14:15 -0300

    Modelar classe StatusContaDTO

---
+++ src/main/java/br/com/zup/GerenciarContas/dtos/StatusContaDTO.java
@@ -0,0 +1,16 @@
+package br.com.zup.GerenciarContas.dtos;
+
+public class StatusContaDTO {
+    private int codigo;
+
+    public StatusContaDTO() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
