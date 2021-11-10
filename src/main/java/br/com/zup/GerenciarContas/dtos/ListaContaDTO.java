9c395a10333a99360a8d709aba845a1f383e0017
Debora Souza <deboral.p@hotmail.com>
2021-11-10 15:05:05 -0300

    Modelar classe ListaContaDTO

---
+++ src/main/java/br/com/zup/GerenciarContas/dtos/ListaContaDTO.java
@@ -0,0 +1,50 @@
+package br.com.zup.GerenciarContas.dtos;
+
+import br.com.zup.GerenciarContas.enuns.Status;
+import br.com.zup.GerenciarContas.enuns.Tipo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ListaContaDTO {
+
+    private int codigo;
+    private String nome;
+    private double valor;
+    private Status status;
+
+    public ListaContaDTO() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
