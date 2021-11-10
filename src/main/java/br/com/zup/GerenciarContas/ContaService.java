75d9575b014bbf87a41cc2aa94f19be65a6e570b
Debora Souza <deboral.p@hotmail.com>
2021-11-10 12:55:58 -0300

    Criar classe ContaService com injecão de dependencia ContaRepository

---
+++ src/main/java/br/com/zup/GerenciarContas/ContaService.java
@@ -0,0 +1,11 @@
+package br.com.zup.GerenciarContas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+    @Autowired
+    private ContaRepository contaRepository;
+
+}
