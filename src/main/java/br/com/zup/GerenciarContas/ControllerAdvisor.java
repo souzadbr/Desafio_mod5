840cf32e9645a1578fc16853baffe65ff6681e6b
Debora Souza <deboral.p@hotmail.com>
2021-11-11 11:21:48 -0300

    Criar método manipularErroDeValidacao na classe ControllerAdvice

--- src/main/java/br/com/zup/GerenciarContas/ControllerAdvisor.java
+++ src/main/java/br/com/zup/GerenciarContas/ControllerAdvisor.java
@@ -1,4 +1,28 @@
 package br.com.zup.GerenciarContas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
 public class ControllerAdvisor {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
+        List<ErroDeValidacao> erros = new ArrayList<>();
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            ErroDeValidacao erroDeValidacao = new ErroDeValidacao(fieldError.getField(),
+                    fieldError.getDefaultMessage());
+            erros.add(erroDeValidacao);
+        }
+        return erros;
+    }
 }
