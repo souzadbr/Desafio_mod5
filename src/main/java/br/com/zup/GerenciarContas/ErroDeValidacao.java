3145e91b8ce40dca7ca08ac077946254cb0d1ea2
Debora Souza <deboral.p@hotmail.com>
2021-11-11 10:56:29 -0300

    Modelar classe ErroDeValidacao

---
+++ src/main/java/br/com/zup/GerenciarContas/ErroDeValidacao.java
@@ -0,0 +1,27 @@
+package br.com.zup.GerenciarContas;
+
+public class ErroDeValidacao {
+    private String mensagem;
+    private String campo;
+
+    public ErroDeValidacao(String mensagem, String campo) {
+        this.mensagem = mensagem;
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+}
